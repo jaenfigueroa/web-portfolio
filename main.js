9204c66a9d490a8139e4ce23389eaa9f3b79ea01
jaen <jaenfigueroa03042000@gmail.com>
2022-12-12 20:10:40 -0500

    header

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//PRIMERA CARGA DE LA PAGINA///////////////////////
 const pantallaDeCarga = document.getElementById('pantalla-de-carga')
 const body = document.getElementById('body')
 
@@ -6,3 +7,36 @@ window.addEventListener('load', () => {
   pantallaDeCarga.style.display = 'none'
   body.classList.remove('over-hidden')
 })
+
+//HEADER/////////////////////////////////////////////////
+const iconoNav = document.getElementById('icono-nav')
+const contenedorNav = document.getElementById('contenedor-nav')
+
+iconoNav.addEventListener('click', () => {
+  contenedorNav.classList.toggle('nav__contenedor--visible')
+})
+
+//SECCION 1///////////////////////////////////////////
+const titulo = document.getElementById('titulo')
+const cargo = document.getElementById('cargo')
+
+function animar() {
+  titulo.classList.add('titulo--animada')
+
+  setTimeout(() => {
+    cargo.classList.add('cargo--animada')
+  }, 2500)
+
+  setTimeout(() => {
+    titulo.classList.remove('titulo--animada')
+    cargo.classList.remove('cargo--animada')
+  }, 7000)
+}
+
+animar()
+
+setInterval(() => {
+  animar()
+}, 8000)
+
+/////////////////////////////////////////////////////
