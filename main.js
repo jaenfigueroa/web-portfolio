3fe87fc364ddb24feed4cfa757eb4d9f51350931
jaen <jaenfigueroa03042000@gmail.com>
2022-12-14 08:07:30 -0500

    add links

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //PRIMERA CARGA DE LA PAGINA///////////////////////
 const pantallaDeCarga = document.getElementById('pantalla-de-carga')
 const body = document.getElementById('body')
+const main = document.getElementById('main')
 
 ///////////////////////////////////////////////////
 window.addEventListener('load', () => {
@@ -13,7 +14,8 @@ const iconoNav = document.getElementById('icono-nav')
 const contenedorNav = document.getElementById('contenedor-nav')
 
 iconoNav.addEventListener('click', () => {
-  contenedorNav.classList.toggle('nav__contenedor--visible')
+  contenedorNav.classList.toggle('nav__caja--visible')
+  // main.classList.toggle('blur')
 })
 
 //SECCION 1///////////////////////////////////////////
