f24f7077a21974cb5db42f0fcab44c2a0d2eb75a
jaen <jaenfigueroa03042000@gmail.com>
2022-12-22 20:10:36 -0500

    archivos actualiados

--- src/redes_trabajos.js
+++ src/redes_trabajos.js
@@ -12,12 +12,29 @@ const REDES_SOCIALES = {
 const TRABAJOS = [
   {
     nombre: 'Rock, Papper or Scissors',
-    fecha: '07 - Oct - 2022',
+    fecha: '01 - Oct - 2022',
     descripcion:
       'Game for 2 people, where you play with your hands, using rock, paper or scissors.',
     website: 'https://jaenfigueroa.github.io/Piedra-Papel-Tijera/',
     github: 'https://github.com/jaenfigueroa/Piedra-Papel-Tijera',
     imagen: './assets/proyectos/ppt.png'
+  },
+  {
+    nombre: 'Google Clone',
+    fecha: '7 - Oct - 2022',
+    descripcion: 'This is a personal project, a clone of the google search engine.',
+    website: 'https://jaenfigueroa.github.io/Google-Buscador/',
+    github: 'https://github.com/jaenfigueroa/Google-Buscador',
+    imagen: './assets/proyectos/google.png'
+  },
+  {
+    nombre: 'De voz a texto',
+    fecha: '15 - Oct - 2022',
+    descripcion:
+      'Application to transcribe the human voice into text, available for the English and Spanish language.',
+    website: 'https://jaenfigueroa.github.io/De-voz-a-texto/',
+    github: 'https://github.com/jaenfigueroa/De-voz-a-texto',
+    imagen: './assets/proyectos/de_voz_a_texto.png'
   },
   {
     nombre: 'To Do List',
@@ -29,6 +46,16 @@ const TRABAJOS = [
     imagen: './assets/proyectos/to_do_list.png'
   },
   {
+    nombre: 'Messaging App',
+    fecha: '31 - Oct - 2022',
+    descripcion:
+      'A real-time messaging application, par puts into practice what they have learned using nodejs, express in the backend.',
+    website: '#',
+    github:
+      'https://github.com/jaenfigueroa/Aplicacion-Chat-de-mensajes-en-tiempo-real',
+    imagen: './assets/proyectos/chat_mensajes.png'
+  },
+  {
     nombre: 'Puzzle Shuerk',
     fecha: '28 - Oct - 2022',
     descripcion:
@@ -36,6 +63,33 @@ const TRABAJOS = [
     website: 'https://jaenfigueroa.github.io/Rompecabezas/',
     github: 'https://github.com/jaenfigueroa/Rompecabezas',
     imagen: './assets/proyectos/puzzle_shuerk.png'
+  },
+  {
+    nombre: 'Halloween Pumpkin',
+    fecha: '1 - Nov - 2022',
+    descripcion:
+      'A personal project for halloween, an animation using only html and css.',
+    website: 'https://jaenfigueroa.github.io/Halloween-Pumpkin/',
+    github: 'https://github.com/jaenfigueroa/Halloween-Pumpkin',
+    imagen: './assets/proyectos/halloween_pumpkin.png'
+  },
+  {
+    nombre: 'Duck and Bread',
+    fecha: '11 - Nov - 2022',
+    descripcion:
+      'A mouse effect, where the duck follows the piece of bread with its eyes.',
+    website: 'https://jaenfigueroa.github.io/Duck-and-bread/',
+    github: 'https://github.com/jaenfigueroa/Duck-and-bread',
+    imagen: './assets/proyectos/duck_and_bread.png'
+  },
+  {
+    nombre: 'Cubo Rubik',
+    fecha: '18 - Nov - 2022',
+    descripcion:
+      "A personal project, an animation of a rubik's cube in 3d using only html and css.",
+    website: 'https://jaenfigueroa.github.io/3D-Rubiks-Cube/',
+    github: 'https://github.com/jaenfigueroa/3D-Rubiks-Cube',
+    imagen: './assets/proyectos/cubo_rubik.png'
   },
   {
     nombre: 'JaenCraft',
@@ -48,12 +102,21 @@ const TRABAJOS = [
   },
   {
     nombre: 'Pomodoro Timer',
-    fecha: '05 - Dec - 2022',
+    fecha: '03 - Dec - 2022',
     descripcion:
       'It is used to take the remaining time, where you can set the minutes and seconds in which an alarm should be displayed.',
     website: 'https://jaenfigueroa.github.io/Pomodoro-timer',
     github: 'https://github.com/jaenfigueroa/Pomodoro-timer',
     imagen: './assets/proyectos/pomodoro_timer.png'
+  },
+  {
+    nombre: 'Pokedex',
+    fecha: '5 - Dec - 2022',
+    descripcion:
+      'The Pokemon app, using the Pokemon API: PokeAPI, has a feature for adding favorites, using local storage.',
+    website: 'https://jaenfigueroa.github.io/PokeDex',
+    github: 'https://github.com/jaenfigueroa/PokeDex',
+    imagen: './assets/proyectos/pokedex.png'
   },
   {
     nombre: 'eCommerce',
@@ -63,6 +126,15 @@ const TRABAJOS = [
     website: 'https://jaenfigueroa.github.io/eCommerce',
     github: 'https://github.com/jaenfigueroa/eCommerce',
     imagen: './assets/proyectos/ecommerce.png'
+  },
+  {
+    nombre: 'Web Portfolio',
+    fecha: '15 - Dec - 2022',
+    descripcion:
+      'Website where different food dishes of different prices and flavors are sold.',
+    website: 'https://jaenfigueroa.github.io/web-portfolio',
+    github: 'https://github.com/jaenfigueroa/web-portfolio',
+    imagen: './assets/proyectos/web_portfolio.png'
   }
 ]
 
