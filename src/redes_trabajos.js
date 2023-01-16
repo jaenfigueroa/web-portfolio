b69d40b2066ff2fc6c4efbd4c415d700af603c80
jaen <jaenfigueroa03042000@gmail.com>
2023-01-16 18:39:22 -0500

    sitio completo en español

--- src/redes_trabajos.js
+++ src/redes_trabajos.js
@@ -14,7 +14,7 @@ const TRABAJOS = [
     nombre: 'Puzzle Shuerk',
     fecha: '28 - Oct - 2022',
     descripcion:
-      'Game where you have to drag and drop to finish assembling the puzzle.',
+      'Juego en el que tienes que arrastrar y soltar para terminar de montar el puzzle.',
     website: 'https://jaenfigueroa.github.io/Rompecabezas/',
     github: 'https://github.com/jaenfigueroa/Rompecabezas',
     imagen: './assets/proyectos/puzzle_shuerk.png'
@@ -23,7 +23,7 @@ const TRABAJOS = [
     nombre: 'Halloween Pumpkin',
     fecha: '1 - Nov - 2022',
     descripcion:
-      'A personal project for halloween, an animation using only html and css.',
+      'Un proyecto personal para halloween, una animación usando solo html y css.',
     website: 'https://jaenfigueroa.github.io/Halloween-Pumpkin/',
     github: 'https://github.com/jaenfigueroa/Halloween-Pumpkin',
     imagen: './assets/proyectos/halloween_pumpkin.png'
@@ -32,7 +32,7 @@ const TRABAJOS = [
     nombre: 'Cubo Rubik',
     fecha: '18 - Nov - 2022',
     descripcion:
-      "A personal project, an animation of a rubik's cube in 3d using only html and css.",
+      "Un proyecto personal, una animación de un cubo de rubik en 3d usando solo html y css.",
     website: 'https://jaenfigueroa.github.io/3D-Rubiks-Cube/',
     github: 'https://github.com/jaenfigueroa/3D-Rubiks-Cube',
     imagen: './assets/proyectos/cubo_rubik.png'
@@ -41,7 +41,7 @@ const TRABAJOS = [
     nombre: 'JaenCraft',
     fecha: '16 - Nov - 2022',
     descripcion:
-      'Minecraft game made only with CSS and Javascript, where you can move the character with the keys, change the person, etc.',
+      'Juego de Minecraft hecho solo con CSS y Javascript, donde puedes mover al personaje con las teclas, cambiar de persona, etc.',
     website: 'https://jaenfigueroa.github.io/JaenCraft/',
     github: 'https://github.com/jaenfigueroa/JaenCraft',
     imagen: './assets/proyectos/jaencraft.png'
@@ -50,7 +50,7 @@ const TRABAJOS = [
     nombre: 'Pomodoro Timer',
     fecha: '03 - Dec - 2022',
     descripcion:
-      'It is used to take the remaining time, where you can set the minutes and seconds in which an alarm should be displayed.',
+      'Se utiliza para tomar el tiempo restante, donde puede establecer los minutos y segundos en los que debe aparecer una alarma.',
     website: 'https://jaenfigueroa.github.io/Pomodoro-timer',
     github: 'https://github.com/jaenfigueroa/Pomodoro-timer',
     imagen: './assets/proyectos/pomodoro_timer.png'
@@ -59,7 +59,7 @@ const TRABAJOS = [
     nombre: 'eCommerce',
     fecha: '10 - Dec - 2022',
     descripcion:
-      'Website where different food dishes of different prices and flavors are sold.',
+      'Sitio web donde se venden platos de comida de diferentes precios y sabores.',
     website: 'https://jaenfigueroa.github.io/eCommerce',
     github: 'https://github.com/jaenfigueroa/eCommerce',
     imagen: './assets/proyectos/ecommerce.png'
@@ -68,7 +68,7 @@ const TRABAJOS = [
     nombre: 'Web Portfolio',
     fecha: '15 - Dec - 2022',
     descripcion:
-      'Website where different food dishes of different prices and flavors are sold.',
+      'Sitio web para mostrar mis habilidades y proyectos como desarrollador.',
     website: 'https://jaenfigueroa.github.io/web-portfolio',
     github: 'https://github.com/jaenfigueroa/web-portfolio',
     imagen: './assets/proyectos/web_portfolio.png'
@@ -77,7 +77,7 @@ const TRABAJOS = [
     nombre: 'Manage Movies List app',
     fecha: '15 - Dec - 2022',
     descripcion:
-      'Application to save movie lists, you can edit them, move them, delete them, search them, etc.',
+      'Aplicación para guardar listas de películas, puedes editarlas, moverlas, borrarlas, buscarlas, etc.',
     website: 'https://manage-movie-list.netlify.app/',
     github: 'https://github.com/jaenfigueroa/manage-movie-list',
     imagen: './assets/proyectos/movies.png'
@@ -86,7 +86,7 @@ const TRABAJOS = [
     nombre: 'PokeZone',
     fecha: '3 - Jan - 2023',
     descripcion:
-      'This project is a Pokémon-themed website created with React.js and the PokeAPI API.',
+      'Este proyecto es un sitio web de temática Pokémon creado con React.js y la API PokeAPI.',
     website: 'https://pokezone.jaenfigueroa.com/',
     github: 'https://github.com/jaenfigueroa/PokeZone',
     imagen: './assets/proyectos/pokezone.png'
